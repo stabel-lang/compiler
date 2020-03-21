9843730f58907a9fcf7f3bbc52eb41df6774ff05
robin.hansen <>
2020-03-21 08:09:27 +0000

    Implement simple qualifier.
    FossilOrigin-Name: 8893ab05d53f82363d7f4a55b5c8d124071bd46cf129917349b695c3ace83b74

--- src/Play/Qualifier.elm
+++ src/Play/Qualifier.elm
@@ -1,107 +1,99 @@
-module Play.Qualifier exposing
-    ( Builtin(..)
-    , Module
-    , Node(..)
-    , TopLevel(..)
-    , qualify
-    )
+module Play.Qualifier exposing (..)
 
 import Dict exposing (Dict)
-import Play.Parser as Parser
+import Play.Parser as AST
+import Set exposing (Set)
 
 
-type alias Module =
-    { ast : Dict String TopLevel }
-
-
-type TopLevel
-    = Def String (List String) (List Node)
-
-
-type Node
-    = BuiltinRef Builtin
-    | LocalRef String
-    | ModuleRef String
-    | ConstInt Int
-
-
-type Builtin
-    = IntAdd
-    | IntSub
-
-
-qualify : Dict String Module -> Parser.Module -> Result String Module
-qualify existingModules moduleAst =
-    let
-        qualifiedAstResult =
-            Dict.foldl qualifyHelp (Ok Dict.empty) moduleAst.ast
-
-        qualifyHelp name node lastResult =
-            case lastResult of
-                Err _ ->
-                    lastResult
-
-                Ok acc ->
-                    case qualifyNode existingModules moduleAst.ast name node of
-                        Ok qualifiedNode ->
-                            Ok <| Dict.insert name qualifiedNode acc
-
-                        Err error ->
-                            Err error
-    in
-    case qualifiedAstResult of
-        Err err ->
-            Err err
-
-        Ok qualifiedAst ->
-            Ok { ast = qualifiedAst }
-
-
-qualifyNode : Dict String Module -> Dict String Parser.TopLevel -> String -> Parser.TopLevel -> Result String TopLevel
-qualifyNode existingModules currentModule name def =
-    case def of
-        Parser.Def _ args nodes ->
-            qualifyDef
-                { otherModules = existingModules
-                , currentModule = currentModule
-                , name = name
-                , args = args
-                }
-                nodes
-                []
-
-
-type alias Context =
-    { otherModules : Dict String Module
-    , currentModule : Dict String Parser.TopLevel
-    , name : String
-    , args : List String
+type alias Definition =
+    { name : String
+    , metadata : List ( String, List AST.AstNode )
+    , implementation : List Node
     }
 
 
-qualifyDef : Context -> List Parser.Node -> List Node -> Result String TopLevel
-qualifyDef context nodes acc =
-    case nodes of
+type Node
+    = Integer Int
+    | Word String
+    | BuiltinPlus
+    | BuiltinMinus
+    | BuiltinEqual
+
+
+builtinDict : Dict String Node
+builtinDict =
+    Dict.fromList
+        [ ( "+", BuiltinPlus )
+        , ( "-", BuiltinMinus )
+        , ( "=", BuiltinEqual )
+        ]
+
+
+qualify : List AST.Definition -> Result () (List Definition)
+qualify ast =
+    let
+        knownUserDefinitions =
+            ast
+                |> List.map .name
+                |> Set.fromList
+    in
+    ast
+        |> List.map (qualifyDefinition knownUserDefinitions)
+        |> listOrError
+
+
+qualifyDefinition : Set String -> AST.Definition -> Result () Definition
+qualifyDefinition knownUserDefinitions definition =
+    let
+        qualifiedImplementationResult =
+            definition.implementation
+                |> List.map (qualifyNode knownUserDefinitions)
+                |> listOrError
+    in
+    case qualifiedImplementationResult of
+        Err () ->
+            Err ()
+
+        Ok qualifiedImplementation ->
+            Ok
+                { name = definition.name
+                , metadata = definition.metadata
+                , implementation = qualifiedImplementation
+                }
+
+
+qualifyNode : Set String -> AST.AstNode -> Result () Node
+qualifyNode knownUserDefinitions node =
+    case node of
+        AST.Integer value ->
+            Ok (Integer value)
+
+        AST.Word value ->
+            if Set.member value knownUserDefinitions then
+                Ok (Word value)
+
+            else
+                case Dict.get value builtinDict of
+                    Just builtin ->
+                        Ok builtin
+
+                    Nothing ->
+                        Err ()
+
+
+listOrError : List (Result () a) -> Result () (List a)
+listOrError list =
+    listOrErrorHelper list []
+
+
+listOrErrorHelper : List (Result () a) -> List a -> Result () (List a)
+listOrErrorHelper potentialTokens tokens =
+    case potentialTokens of
         [] ->
-            Ok <| Def context.name context.args (List.reverse acc)
+            Ok (List.reverse tokens)
 
-        first :: rest ->
-            case first of
-                Parser.Integer value ->
-                    qualifyDef context rest (ConstInt value :: acc)
+        (Err ()) :: _ ->
+            Err ()
 
-                Parser.Symbol "builtin_plus" ->
-                    qualifyDef context rest (BuiltinRef IntAdd :: acc)
-
-                Parser.Symbol "builtin_sub" ->
-                    qualifyDef context rest (BuiltinRef IntSub :: acc)
-
-                Parser.Symbol ref ->
-                    if List.member ref context.args then
-                        qualifyDef context rest (LocalRef ref :: acc)
-
-                    else if Dict.get ref context.currentModule /= Nothing then
-                        qualifyDef context rest (ModuleRef ref :: acc)
-
-                    else
-                        Err <| "Could not find reference '" ++ ref ++ "'"
+        (Ok token) :: remaining ->
+            listOrErrorHelper remaining (token :: tokens)
--- tests/Test/Qualifier.elm
+++ tests/Test/Qualifier.elm
@@ -1,10 +1,81 @@
 module Test.Qualifier exposing (..)
 
 import Expect
+import Play.Parser as AST
+import Play.Qualifier exposing (..)
 import Test exposing (Test, describe, test, todo)
 
 
 suite : Test
 suite =
-    describe "Qualifying parsed modules"
-        [ todo "todo" ]
+    describe "Qualifier"
+        [ test "Simple program" <|
+            \_ ->
+                let
+                    sourceDefinitions =
+                        [ { name = "inc"
+                          , metadata = []
+                          , implementation =
+                                [ AST.Integer 1
+                                , AST.Word "+"
+                                ]
+                          }
+                        , { name = "dec"
+                          , metadata = []
+                          , implementation =
+                                [ AST.Integer 1
+                                , AST.Word "-"
+                                ]
+                          }
+                        , { name = "main"
+                          , metadata =
+                                [ ( "entry", [ AST.Word "true" ] )
+                                ]
+                          , implementation =
+                                [ AST.Integer 1
+                                , AST.Word "inc"
+                                , AST.Word "inc"
+                                , AST.Word "dec"
+                                , AST.Integer 2
+                                , AST.Word "="
+                                ]
+                          }
+                        ]
+
+                    expectedDefinitions =
+                        [ { name = "inc"
+                          , metadata = []
+                          , implementation =
+                                [ Integer 1
+                                , BuiltinPlus
+                                ]
+                          }
+                        , { name = "dec"
+                          , metadata = []
+                          , implementation =
+                                [ Integer 1
+                                , BuiltinMinus
+                                ]
+                          }
+                        , { name = "main"
+                          , metadata =
+                                [ ( "entry", [ AST.Word "true" ] )
+                                ]
+                          , implementation =
+                                [ Integer 1
+                                , Word "inc"
+                                , Word "inc"
+                                , Word "dec"
+                                , Integer 2
+                                , BuiltinEqual
+                                ]
+                          }
+                        ]
+                in
+                case qualify sourceDefinitions of
+                    Err () ->
+                        Expect.fail "Did not expect qualification to fail"
+
+                    Ok definitions ->
+                        Expect.equalLists expectedDefinitions definitions
+        ]
