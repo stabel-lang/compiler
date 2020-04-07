31967db3a66d1d29a52050119cef63033c9ce5b5
robin.hansen <>
2020-04-07 11:16:10 +0000

    Add more numeric and stack manipulation builtins.
    FossilOrigin-Name: 728d07fba452dede1291312c0af8fba6a120a030aa65eea1a66cdf9eaee79d92

--- src/Play/Data/Builtin.elm
+++ src/Play/Data/Builtin.elm
@@ -6,17 +6,65 @@ import Play.Data.Type as Type exposing (WordType)
 type Builtin
     = Plus
     | Minus
+    | Multiply
+    | Divide
     | Equal
+    | StackDuplicate
+    | StackDrop
+    | StackSwap
+    | StackRightRotate
+    | StackLeftRotate
 
 
 wordType : Builtin -> WordType
 wordType builtin =
     case builtin of
         Plus ->
-            { input = [ Type.Int, Type.Int ], output = [ Type.Int ] }
+            { input = [ Type.Int, Type.Int ]
+            , output = [ Type.Int ]
+            }
 
         Minus ->
-            { input = [ Type.Int, Type.Int ], output = [ Type.Int ] }
+            { input = [ Type.Int, Type.Int ]
+            , output = [ Type.Int ]
+            }
+
+        Multiply ->
+            { input = [ Type.Int, Type.Int ]
+            , output = [ Type.Int ]
+            }
+
+        Divide ->
+            { input = [ Type.Int, Type.Int ]
+            , output = [ Type.Int ]
+            }
 
         Equal ->
-            { input = [ Type.Int, Type.Int ], output = [ Type.Int ] }
+            { input = [ Type.Int, Type.Int ]
+            , output = [ Type.Int ]
+            }
+
+        StackDuplicate ->
+            { input = [ Type.Generic "a" ]
+            , output = [ Type.Generic "a", Type.Generic "a" ]
+            }
+
+        StackDrop ->
+            { input = [ Type.Generic "a" ]
+            , output = []
+            }
+
+        StackSwap ->
+            { input = [ Type.Generic "a", Type.Generic "b" ]
+            , output = [ Type.Generic "b", Type.Generic "a" ]
+            }
+
+        StackRightRotate ->
+            { input = [ Type.Generic "a", Type.Generic "b", Type.Generic "c" ]
+            , output = [ Type.Generic "c", Type.Generic "a", Type.Generic "b" ]
+            }
+
+        StackLeftRotate ->
+            { input = [ Type.Generic "a", Type.Generic "b", Type.Generic "c" ]
+            , output = [ Type.Generic "b", Type.Generic "c", Type.Generic "a" ]
+            }
