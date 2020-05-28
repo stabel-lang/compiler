26e3a05852f247340a3ce0b8b85680aa2c5311d7
robin.hansen <>
2020-05-28 15:29:03 +0000

    Use elm/parser for tokenizer.
    FossilOrigin-Name: eb865aaf6eac0a40f96f8a487d6ebaae786f59f5ca2b63b92709e9975ecd191d

--- src/Play/Tokenizer.elm
+++ src/Play/Tokenizer.elm
@@ -1,6 +1,11 @@
-module Play.Tokenizer exposing (..)
+module Play.Tokenizer exposing
+    ( Token(..)
+    , tokenize
+    )
 
+import Parser exposing ((|.), (|=), Parser)
 import Result.Extra as Result
+import Set exposing (Set)
 
 
 type Token
@@ -19,64 +24,142 @@ type Token
 
 tokenize : String -> Result () (List Token)
 tokenize sourceCode =
-    sourceCode
-        |> String.words
-        |> List.map recognizeToken
-        |> Result.combine
+    Parser.run parser sourceCode
+        |> Result.mapError (Debug.log "err")
+        |> Result.mapError (always ())
 
 
-recognizeToken : String -> Result () Token
-recognizeToken word =
-    case String.toInt word of
-        Just value ->
-            Ok (Integer value)
-
-        Nothing ->
-            if stringStartsWithUpper word then
-                if String.endsWith "(" word then
-                    word
-                        |> String.dropRight 1
-                        |> PatternMatchStart
-                        |> Ok
-
-                else
-                    Ok (Type word)
-
-            else if String.endsWith ":" word then
-                word
-                    |> String.dropRight 1
-                    |> Metadata
-                    |> Ok
-
-            else
-                case word of
-                    "--" ->
-                        Ok TypeSeperator
-
-                    "{" ->
-                        Ok ListStart
-
-                    "}" ->
-                        Ok ListEnd
-
-                    "[" ->
-                        Ok QuoteStart
-
-                    "]" ->
-                        Ok QuoteStop
-
-                    ")" ->
-                        Ok ParenStop
-
-                    _ ->
-                        Ok (Symbol word)
+parser : Parser (List Token)
+parser =
+    Parser.succeed identity
+        |. Parser.spaces
+        |= Parser.loop [] parserHelp
+        |. Parser.spaces
 
 
-stringStartsWithUpper : String -> Bool
-stringStartsWithUpper str =
-    case String.uncons str of
-        Just ( firstLetter, _ ) ->
-            Char.isUpper firstLetter
 
-        Nothing ->
-            False
+--|. Parser.end
+
+
+parserHelp : List Token -> Parser (Parser.Step (List Token) (List Token))
+parserHelp reverseTokens =
+    Parser.oneOf
+        [ Parser.succeed (\token -> Parser.Loop (token :: reverseTokens))
+            |= tokenParser
+            |. Parser.spaces
+        , Parser.succeed ()
+            |> Parser.map (\_ -> Parser.Done (List.reverse reverseTokens))
+        ]
+
+
+tokenParser : Parser Token
+tokenParser =
+    Parser.oneOf
+        [ intParser
+            |> Parser.map Integer
+        , Parser.succeed (Metadata "")
+            |. Parser.symbol ":"
+        , Parser.succeed ListStart
+            |. Parser.symbol "{"
+        , Parser.succeed ListEnd
+            |. Parser.symbol "}"
+        , Parser.succeed QuoteStart
+            |. Parser.symbol "["
+        , Parser.succeed QuoteStop
+            |. Parser.symbol "]"
+        , Parser.succeed ParenStop
+            |. Parser.symbol ")"
+        , Parser.succeed TypeSeperator
+            |. Parser.symbol "--"
+        , typeParser
+        , symbolParser
+        ]
+
+
+intParser : Parser Int
+intParser =
+    -- The builtin int parser has a bug where it commits when it comes across an 'e'
+    let
+        helper text =
+            case String.toInt text of
+                Just num ->
+                    Parser.succeed num
+
+                Nothing ->
+                    Parser.problem "Not a valid integer"
+    in
+    Parser.variable
+        { start = Char.isDigit
+        , inner = Char.isDigit
+        , reserved = Set.empty
+        }
+        |> Parser.andThen helper
+
+
+typeParser : Parser Token
+typeParser =
+    let
+        helper text =
+            Parser.oneOf
+                [ Parser.succeed (PatternMatchStart text)
+                    |. Parser.chompIf (\c -> c == '(')
+                , Parser.succeed (Type text)
+                ]
+    in
+    Parser.variable
+        { start = Char.isUpper
+        , inner = validSymbolChar
+        , reserved = Set.empty
+        }
+        |> Parser.andThen helper
+
+
+symbolParser : Parser Token
+symbolParser =
+    let
+        helper text =
+            Parser.oneOf
+                [ Parser.succeed (Metadata text)
+                    |. Parser.chompIf (\c -> c == ':')
+                , Parser.succeed (Symbol text)
+                ]
+    in
+    Parser.variable
+        { start = validSymbolChar
+        , inner = validSymbolChar
+        , reserved = Set.empty
+        }
+        |> Parser.andThen helper
+
+
+validSymbolChar : Char -> Bool
+validSymbolChar c =
+    not <| Set.member c invalidSymbolChars
+
+
+invalidSymbolChars : Set Char
+invalidSymbolChars =
+    Set.union whitespaceChars specialChars
+
+
+specialChars : Set Char
+specialChars =
+    Set.fromList
+        [ ':'
+        , '{'
+        , '}'
+        , '['
+        , ']'
+        , '('
+        , ')'
+        ]
+
+
+whitespaceChars : Set Char
+whitespaceChars =
+    Set.fromList
+        [ ' '
+        , '\n'
+        , '\u{000D}'
+        , '\t'
+        ]
