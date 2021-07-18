b337a4ffcd4b2528781015e887152a0f14a435d0
robin.hansen <>
2021-07-18 10:39:38 +0000

    Convert several unit tests to integration tests.
    FossilOrigin-Name: a52bc7b6b29c6025c99d189433ff3d6d59a92dd71a4084ac70dce829b05dc623

--- wasm_tests/module_resolution.test.js
+++ wasm_tests/module_resolution.test.js
@@ -179,3 +179,295 @@ test('External package types', async () => {
 
     expect(result.stackElement()).toBe(200);
 });
+
+test('Module aliases', async () => {
+    const entryPoint = '/author/sample/core/main'
+    const wat = await compiler.toProjectWat({
+        entryPoint: entryPoint, 
+        modules: [
+            {
+                package: 'author/external',
+                module: 'tipe',
+                source: `
+                    defstruct: Tipe
+                `
+            },
+            {
+                package: 'author/external',
+                module: 'mod',
+                source: `
+                    def: add
+                    : +
+                `
+            },
+            {
+                package: 'author/sample',
+                module: 'internal/tope',
+                source: `
+                    defstruct: Tope
+                `
+            },
+            {
+                package: 'author/sample',
+                module: 'internal/mod',
+                source: `
+                    def: value
+                    : 2
+                `
+            },
+            {
+                package: 'author/sample',
+                module: 'core',
+                source: `
+                    defmodule:
+                    alias: ext /mod
+                    alias: tope internal/tope
+                    :
+
+                    defstruct: TipeTope
+                    : first tope/Tope
+
+                    def: main
+                    alias: internal internal/mod
+                    : 6 
+                      internal/value
+                      ext/add
+
+                    def: noop
+                    alias: tipe /tipe
+                    type: tipe/Tipe tope/Tope --
+                    : drop drop
+
+                    defmulti: as-int
+                    alias: tipe /tipe
+                    : tipe/Tipe
+                      drop 1
+                    : tope/Tope
+                      drop 2
+                `
+            },
+        ]
+    });
+
+    const result = await compiler.run(wat, entryPoint);
+
+    expect(result.stackElement()).toBe(8);
+});
+
+test('Module imports', async () => {
+    const entryPoint = '/author/sample/core/main'
+    const wat = await compiler.toProjectWat({
+        entryPoint: entryPoint, 
+        modules: [
+            {
+                package: 'author/external',
+                module: 'tipe',
+                source: `
+                    defstruct: Tipe
+                `
+            },
+            {
+                package: 'author/external',
+                module: 'mod',
+                source: `
+                    def: add
+                    : +
+                `
+            },
+            {
+                package: 'author/sample',
+                module: 'internal/tope',
+                source: `
+                    defstruct: Tope
+                `
+            },
+            {
+                package: 'author/sample',
+                module: 'internal/mod',
+                source: `
+                    def: value
+                    : 2
+                `
+            },
+            {
+                package: 'author/sample',
+                module: 'core',
+                source: `
+                    defmodule:
+                    import: /mod add
+                    import: internal/tope
+                    :
+
+                    defstruct: TipeTope
+                    : first Tope
+
+                    def: main
+                    import: internal/mod
+                    : 6 
+                      value
+                      add
+
+                    def: noop
+                    import: /tipe
+                    type: Tipe Tope --
+                    : drop drop
+
+                    defmulti: as-int
+                    import: /tipe
+                    : Tipe
+                      drop 1
+                    : Tope
+                      drop 2
+                `
+            },
+        ]
+    });
+
+    const result = await compiler.run(wat, entryPoint);
+
+    expect(result.stackElement()).toBe(8);
+});
+
+test('Anonymous module exposes everything', async () => {
+    const entryPoint = '/author/sample/core/main'
+    const wat = await compiler.toProjectWat({
+        entryPoint: entryPoint, 
+        modules: [
+            {
+                package: 'author/external',
+                module: 'ext',
+                source: `
+                    def: fn1
+                    : 1 2 +
+                `
+            },
+            {
+                package: 'author/sample',
+                module: 'internal',
+                source: `
+                    def: fn2
+                    : 3 5 +
+                `
+            },
+            {
+                package: 'author/sample',
+                module: 'core',
+                source: `
+                    def: main
+                    : /ext/fn1 internal/fn2 +                 `
+            },
+        ]
+    });
+
+    const result = await compiler.run(wat, entryPoint);
+
+    expect(result.stackElement()).toBe(11);
+});
+
+test('Defined module requires explicit exports', async () => {
+    const entryPoint = '/author/sample/core/main'
+    const wat = await compiler.toProjectWat({
+        entryPoint: entryPoint, 
+        modules: [
+            {
+                package: 'author/external',
+                module: 'ext',
+                source: `
+                    defmodule:
+                    expose: fn1
+                    :
+
+                    def: fn1
+                    : 1 2 +
+                `
+            },
+            {
+                package: 'author/sample',
+                module: 'internal',
+                source: `
+                    defmodule:
+                    expose: fn2
+                    :
+
+                    def: fn2
+                    : 3 5 +
+                `
+            },
+            {
+                package: 'author/sample',
+                module: 'core',
+                source: `
+                    def: main
+                    : /ext/fn1 internal/fn2 +                 `
+            },
+        ]
+    });
+
+    const result = await compiler.run(wat, entryPoint);
+
+    expect(result.stackElement()).toBe(11);
+});
+
+test('Implicit import on stdlib in anonymous modules', async () => {
+    const entryPoint = '/author/sample/core/main'
+    const wat = await compiler.toProjectWat({
+        entryPoint: entryPoint, 
+        modules: [
+            {
+                package: 'stabel/standard_library',
+                module: 'core',
+                source: `
+                    def: over
+                    type: a b -- a b a
+                    : swap dup rotate
+                `
+            },
+            {
+                package: 'author/sample',
+                module: 'core',
+                source: `
+                    def: main
+                    : 1 2 over + +
+                `
+            },
+        ]
+    });
+
+    const result = await compiler.run(wat, entryPoint);
+
+    expect(result.stackElement()).toBe(4);
+});
+
+test('Implicit import on stdlib in defined modules', async () => {
+    const entryPoint = '/author/sample/core/main'
+    const wat = await compiler.toProjectWat({
+        entryPoint: entryPoint, 
+        modules: [
+            {
+                package: 'stabel/standard_library',
+                module: 'core',
+                source: `
+                    def: over
+                    type: a b -- a b a
+                    : swap dup rotate
+                `
+            },
+            {
+                package: 'author/sample',
+                module: 'core',
+                source: `
+                    defmodule:
+                    expose: main
+                    :
+
+                    def: main
+                    : 1 2 over + +
+                `
+            },
+        ]
+    });
+
+    const result = await compiler.run(wat, entryPoint);
+
+    expect(result.stackElement()).toBe(4);
+});
