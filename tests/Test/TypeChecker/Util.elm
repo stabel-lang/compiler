3fff84034b192bb2f2fe0704a720f1c763c88b0c
robin.hansen <>
2021-07-22 11:20:16 +0000

    Utility functions in Test.TypeChecker.Util now takes source code instead of qualified AST as input.
    FossilOrigin-Name: 0f53db66fd0a496f0626822c0b7afa33cfa2e5fba45ace6465a387f86b09821d

--- tests/Test/TypeChecker/Util.elm
+++ tests/Test/TypeChecker/Util.elm
@@ -6,35 +6,77 @@ module Test.TypeChecker.Util exposing
 
 import Dict exposing (Dict)
 import Expect exposing (Expectation)
-import Stabel.Qualifier as QAST
-import Stabel.TypeChecker exposing (FunctionDefinition, run)
+import Stabel.Parser as Parser
+import Stabel.Qualifier as Qualifier
+import Stabel.TypeChecker as TypeChecker exposing (FunctionDefinition)
+import Test.Qualifier.Util as QualifierUtil
 
 
-expectTypeCheck : QAST.AST -> Expectation
+expectTypeCheck : String -> Expectation
 expectTypeCheck input =
-    case run input of
+    case qualifyInput input of
         Err err ->
-            Expect.fail <| "Did not expect typecheck to fail: " ++ Debug.toString err
+            Expect.fail err
 
-        Ok _ ->
-            Expect.pass
+        Ok qualifiedAst ->
+            case TypeChecker.run qualifiedAst of
+                Err err ->
+                    Expect.fail <| "Did not expect typecheck to fail: " ++ Debug.toString err
+
+                Ok _ ->
+                    Expect.pass
 
 
-expectTypeCheckFailure : QAST.AST -> Expectation
+expectTypeCheckFailure : String -> Expectation
 expectTypeCheckFailure input =
-    case run input of
-        Err _ ->
-            Expect.pass
-
-        Ok _ ->
-            Expect.fail "Did not expect type check to succeed."
-
-
-expectAst : QAST.AST -> Dict String FunctionDefinition -> Expectation
-expectAst input expectedResult =
-    case run input of
+    case qualifyInput input of
         Err err ->
-            Expect.fail <| "Did not expect typecheck to fail: " ++ Debug.toString err
+            Expect.fail err
 
-        Ok typedAst ->
-            Expect.equal expectedResult typedAst.functions
+        Ok qualifiedAst ->
+            case TypeChecker.run qualifiedAst of
+                Err _ ->
+                    Expect.pass
+
+                Ok _ ->
+                    Expect.fail "Did not expect type check to succeed."
+
+
+expectAst : String -> Dict String FunctionDefinition -> Expectation
+expectAst input expectedResult =
+    case qualifyInput input of
+        Err err ->
+            Expect.fail err
+
+        Ok qualifiedAst ->
+            case TypeChecker.run qualifiedAst of
+                Err err ->
+                    Expect.fail <| "Did not expect typecheck to fail: " ++ Debug.toString err
+
+                Ok typedAst ->
+                    Expect.equal expectedResult typedAst.functions
+
+
+qualifyInput : String -> Result String Qualifier.AST
+qualifyInput source =
+    case Parser.run "test" source of
+        Err err ->
+            Err <| "Parse error: " ++ Debug.toString err
+
+        Ok parserAst ->
+            let
+                qualifierResult =
+                    Qualifier.run
+                        { packageName = ""
+                        , modulePath = ""
+                        , ast = parserAst
+                        , externalModules = Dict.empty
+                        , inProgressAST = QualifierUtil.emptyAst
+                        }
+            in
+            case qualifierResult of
+                Err err ->
+                    Err <| "Qualifier error: " ++ Debug.toString err
+
+                Ok qualifiedAst ->
+                    Ok qualifiedAst
