58bf3c8e8b052520b6d06d4ffcba2551393b226d
robin.hansen <>
2021-08-01 09:17:11 +0000

    Added elm-review config, and updated other dependencies.
    FossilOrigin-Name: 3d05a90b00530de4bc38d5e716d264a994edbcba71cff2cfb040e1f7f602e8b7

---
+++ review/src/ReviewConfig.elm
@@ -0,0 +1,26 @@
+module ReviewConfig exposing (config)
+
+import NoUnused.CustomTypeConstructorArgs
+import NoUnused.CustomTypeConstructors
+import NoUnused.Dependencies
+import NoUnused.Exports
+import NoUnused.Modules
+import NoUnused.Parameters
+import NoUnused.Patterns
+import NoUnused.Variables
+import Review.Rule exposing (Rule)
+import Simplify
+
+
+config : List Rule
+config =
+    [ Simplify.rule Simplify.defaults
+    , NoUnused.CustomTypeConstructors.rule []
+    , NoUnused.CustomTypeConstructorArgs.rule
+    , NoUnused.Dependencies.rule
+    , NoUnused.Exports.rule
+    , NoUnused.Modules.rule
+    , NoUnused.Parameters.rule
+    , NoUnused.Patterns.rule
+    , NoUnused.Variables.rule
+    ]
